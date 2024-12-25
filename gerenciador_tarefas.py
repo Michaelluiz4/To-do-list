2834d463405666bc9a25580d2987366d030d99c5
Michael <michaelsb065@gmail.com>
2024-12-25 01:05:03 -0300

    Refatoração

--- gerenciador_tarefas.py
+++ gerenciador_tarefas.py
@@ -1,189 +1,78 @@
-class Tarefa:
-    def __init__(self, nome, descricao=None, status='Pendente'):
-        self.nome = nome
-        self.descricao = descricao
-        self.status = status
-        self.concluida = False
-
+from datetime import datetime
+import sqlite3
 
 class GerenciadorTarefa:
     def __init__(self):
-        self.lista_tarefas = []
-        self.carregar_tarefas_do_arquivo()
+        self.conexao = sqlite3.connect('tarefas.db')
+        self.cursor = self.conexao.cursor()
+        self.criar_tabela()
 
 
-    def linhas(self):
-        print('-=' * 40)
-
-
-    def carregar_tarefas_do_arquivo(self):
-        """Carrega as tarefas salvas no arquivo evitando duplicações"""
-        try:
-            with open('Tarefas.txt', 'r', encoding='utf-8') as arquivo:
-                for linha in arquivo:
-                    if linha.startswith('Tarefa'):
-                        partes = linha.strip().split(' - ')
-                        nome_tarefa = partes[0].split(':', 1)[1].strip()
-                        descricao = (
-                            partes[1].strip() if len(partes) > 2 else None
-                        )
-                        status = (
-                            partes[2].strip() if len(partes) > 2 else 'Pendente'
-                        )
-                        if (
-                            nome_tarefa
-                            not in [tarefa.nome for tarefa in self.lista_tarefas]
-                        ):
-                            nova_tarefa = Tarefa(nome_tarefa, descricao, status)
-                            self.lista_tarefas.append(nova_tarefa)
-        except FileNotFoundError:
-            print('Arquivo de tarefas não encontrado criando um novo... ')
-
-
-    def salvar_tarefas_no_arquivo(self):
-        """Salva as tarefas no arquivo Tarefas.txt"""
-        with open('Tarefas.txt', 'w', encoding='utf-8') as arquivo:
-            for tarefa in self.lista_tarefas:
-                descricao = tarefa.descricao if tarefa.descricao else ''
-                arquivo.write(
-                    f'Tarefa: {tarefa.nome} - {descricao} - {tarefa.status}\n'
+    def criar_tabela(self):
+        """Criar a tabela de tarefas no banco de dados"""
+        self.cursor.execute('''
+                CREATE TABLE IF NOT EXISTS tarefas (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome TEXT NOT NULL,
+                    descricao TEXT,
+                    status TEXT NOT NULL DEFAULT 'Pendente'            
                 )
+        ''')
+        self.conexao.commit()
 
 
-    def adicionar_tarefa(self):
-        """Adicionar tarefa a lista de tarefas"""
-        while True:
-            self.linhas()
-
-            nome_tarefa = (
-                input(f'Qual tarefa você vai adicionar a lista: ')
-                .strip()
-                .lower()
-            )
-            if nome_tarefa != 'nenhuma':
-                descricao = None            
-                if nome_tarefa  not in[tarefa.nome for tarefa in self.lista_tarefas]:
-                    descricao_opcional = (
-                        input('\033[34mDESCRIÇÃO\033[0m: Você deseja adicionar '
-                              'uma descrição? (Opcional) [Sim/Não]: ')
-                              .strip()
-                              .lower()
-                    )
-                    if descricao_opcional == 'sim':
-                        descricao = input('Descrição: ').strip().lower()
-                    nova_tarefa = Tarefa(nome_tarefa, descricao)
-                    self.lista_tarefas.append(nova_tarefa)
-                    print(f'Tarefa "{nome_tarefa}" adicionada com sucesso. ')
-
-                    self.salvar_tarefas_no_arquivo()
-                    
-                    with open('Tarefas.txt', 'a', encoding='utf-8') as arquivo:
-                        arquivo.write(
-                            f'Tarefa: {nova_tarefa.nome} - '
-                            f'{nova_tarefa.descricao} - {nova_tarefa.status}\n')
-                else:
-                     print('Tarefa já está na lista de tarefas. ')
-                finalizar = (
-                    input('\033[32mADICIONAR\033[0m: Você deseja adicionar '  
-                          'outra tarefa? [Sim/Não]: ')
-                          .strip()
-                          .lower()
-                )
-                if finalizar in ['não', 'nao']:
-                    break
-                elif finalizar == 'sim':
-                    continue
-                elif finalizar != 'sim':
-                    print('\033[31mERRO\033[0m: Opção invalída. ')
-            break      
+    def adicionar_tarefa(self, nome, descricao=None):
+        """Adicionar uma tarefa no banco de dados"""
+        self.cursor.execute('''
+            INSERT INTO tarefas (nome, descricao) VALUES (?, ?)
+        ''', (nome, descricao))
+        self.conexao.commit()
+        print(f'Tarefa "{nome}" adicionada com sucesso.')
 
 
-    def remover_tarefa(self):
-        """Úsuario remove uma tarefa (caso ele queira) do arquivo Tarefas.txt"""
-        opcao = (
-            input('\033[33mREMOVER\033[0m: Você deseja remover alguma tarefa? ' 
-                  '[Sim/Não]: ').strip().lower()
-        )
-        if opcao == 'sim':
-            tarefa_a_remover = (
-                input('Digite o nome ou ID da tarefa que você deseja remover: ')
-                .strip()
-                .lower()
-            )
-            try:
-                indice = int(tarefa_a_remover)
-                if 1 <= indice <= len(self.lista_tarefas):
-                    tarefa_removida = self.lista_tarefas.pop(indice-1)
-                    print(f'Tarefa {tarefa_removida.nome} removida com sucesso.')
-                    self.salvar_tarefas_no_arquivo()
-                else:
-                    print(
-                        f'\033[31mERRO\033[0m: Não existe uma tarefa '
-                        f'com o número {indice} na lista.')
-            except ValueError:    
-                for tarefa in self.lista_tarefas:
-                    if tarefa.nome == tarefa_a_remover:    
-                        self.lista_tarefas.remove(tarefa)
-                        print(f'Tarefa {tarefa_a_remover} removida com sucesso.')
-                        self.salvar_tarefas_no_arquivo()
-                        return        
-                print(
-                    f'\033[31mErro\033[0m: A tarefa {tarefa_a_remover}' 
-                    'não foi encontrada na lista. ')
-        elif opcao not in ['não', 'nao']:
-            print('\033[31mErro\033[0m: Opção invalída. ')
+    def listar_tarefas(self):
+        """"Lista todas as tarefas do banco de dados"""
+        self.cursor.execute('SELECT id, nome, descricao, status FROM tarefas')
+        tarefas = self.cursor.fetchall()
 
-
-    def exibir_tarefas(self):
-        """Exibir as tarefas"""
-        self.linhas()
-        if not self.lista_tarefas:
-            print('Nenhuma tarefa na lista')
+        if not tarefas:
+            print('Nenhuma tarefa encontrada. ')
         else:
-            print('Suas tarefas são: ')
-            for i, tarefa in enumerate(self.lista_tarefas, start=1):
-                self.status = (
-                    'Concluída' if tarefa.concluida else 'Não concluída'
-                )
-                descricao = f'- {tarefa.descricao}' if tarefa.descricao else ''
+            print('Suas tarefas: ')
+            for tarefa in tarefas:
+                id_tarefa, nome, descricao, status = tarefa
+                descricao = descricao if descricao else 'Tarefa sem descrição'
                 print(
-                    f'{i} - {tarefa.nome} {descricao} - Status: {tarefa.status}'
-                ) 
-        self.linhas()
+                    f'ID: {id_tarefa} - Nome: {nome} - Descrição: {descricao}'
+                    f'Status: {status}'
+                )
 
+    def remover_tarefa(self, id_tarefa):
+        """Remove uma tarefa do banco de dados pelo ID"""
+        self.cursor.execute('DELETE FROM tarefas WHERE id = ?', (id_tarefa,))
+        self.conexao.commit()
 
-    def marcar_como_concluida(self):
-        """Marcar as tarefas como concluídas"""
-        tarefa_concluida = (
-            input('Digite o nome da tarefa que você já concluíu: ')
-            .strip()
-            .lower()
-        )
+        if self.cursor.rowcount > 0:
+            print(f'Tarefa ID: {id_tarefa} removida com sucesso. ')
+        else:
+            print(f'Erro: Tarefa ID {id_tarefa} não encontrada. ')
+    
+
+    def marcar_como_concluida(self, id_tarefa):
+        """Marcar uma tarefaa como concluída no banco de dados"""
+        self.cursor.execute('''
+            UPDATE tarefas
+            SET status = 'Concluída'
+            WHERE id = ? AND status != 'Concluída'
+        ''', (id_tarefa,))
+        self.conexao.commit()
+
+        if self.cursor.rowcount > 0:
+            print(f'Tarefa ID {id_tarefa} marcada como concluída')
+        else:
+            print(f'Erro: Tarefa ID {id_tarefa} não encontrada ou já concluída.')
+
+    def fechar_conexao(self):
+        """Fechar a conexão com o banco de dados"""
+        self.conexao.close()
         
-        if tarefa_concluida != 'nenhuma':
-            for tarefa in self.lista_tarefas:
-                if tarefa.nome.lower() == tarefa_concluida:
-                    if not tarefa.concluida:
-                        tarefa.concluida = True
-                        tarefa.status = 'Concluída'
-                        print(
-                            f'Tarefa {tarefa_concluida} marcada como concluída.'
-                        )
-                        self.salvar_tarefas_no_arquivo()
-                        return
-                    else:
-                        print(
-                            f'Tarefa {tarefa_concluida}'
-                             'já está marcada como concluída. '
-                        )
-                        return
-
-
-"""
-gerenciador = GerenciadorTarefa()
-gerenciador.adicionar_tarefa()
-gerenciador.remover_tarefa()
-gerenciador.exibir_tarefas()
-gerenciador.marcar_como_concluida()
-gerenciador.exibir_tarefas()
-"""
--- main.py
+++ main.py
@@ -2,8 +2,38 @@ from gerenciador_tarefas import GerenciadorTarefa
 
 if __name__ == '__main__':
     gerenciador = GerenciadorTarefa()
-    gerenciador.adicionar_tarefa()  
-    gerenciador.remover_tarefa()
-    gerenciador.exibir_tarefas()
-    gerenciador.marcar_como_concluida()
-    gerenciador.exibir_tarefas()
+    
+    while True:
+        print('\nMenu de gerenciamento de tarefas:')
+        print('1. Adicionar tarefa')
+        print('2. Listar tarefas')
+        print('3. Remover tarefa')
+        print('4. Marcar tarefa como concluída')
+        print('5. Sair')
+
+        opcao = input('Escolha uma opção: ').strip()
+
+        if opcao == '1':
+            nome = input('Nome da tarefa: ').strip()
+            descricao = input('Descrição da tarefa (Opcional): ').strip() or None
+            gerenciador.adicionar_tarefa(nome, descricao)
+        elif opcao == '2':
+            gerenciador.listar_tarefas()
+        elif opcao == '3':
+            try:
+                id_tarefa = int(input('ID da tarefa a remover: ').strip())
+                gerenciador.remover_tarefa(id_tarefa)
+            except ValueError:
+                print('Erro o ID deve ser um número inteiro. ')
+        elif opcao == '4':
+            try:
+                id_tarefa = int(input('ID da tarefa a ser marcada como concluída '))
+                gerenciador.marcar_como_concluida(id_tarefa)
+            except ValueError:
+                print('Erro o ID deve ser um número inteiro. ')
+        elif opcao == '5':
+            gerenciador.fechar_conexao()
+            print('Saindo do programa. Até logo! ')
+            break
+        else:
+            print('\033[31mErro\033[0m: Opção invalída. Tente novamente. ')
